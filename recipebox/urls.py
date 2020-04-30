2620de2a9a11c1cce173367f209872d456df6c5d
Janell.Huyck <Janell.Huyck@outlook.com>
2020-04-30 18:15:07 -0400

    added recipes urlpatterns and connected to root urls

--- recipebox/urls.py
+++ recipebox/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from recipes.urls import urlpatterns as recipes_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += recipes_urls
