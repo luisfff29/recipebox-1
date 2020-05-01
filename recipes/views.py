0fea3e744a00f9ac3c78b97d9cb49aea0879b38f
Janell.Huyck <Janell.Huyck@outlook.com>
2020-05-01 16:41:11 -0400

    made basic base block and basic css set-up

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from recipes.models import Source, Recipe
 
+sources = Source.objects.all()
+
+# source_recipes = Recipe.objects.filter(source=source).order_by('title')
+
+# <p > {{post.text | linebreaksbr}} < /p >   -- for template
+
 
 def index(request):
-    recipes = Recipe.objects.all()
-    sources = Source.objects.all()
-    return render(request, 'index.html', {'recipes': recipes,
-                                          'sources': sources})
+    recipes = Recipe.objects.all().order_by('title')
+    return render(request, 'recipes/index.html', {'recipes': recipes, })
+
+
+# def recipe_detail(request, pk):
+#     recipe = Recipe.objects.get(pk)
